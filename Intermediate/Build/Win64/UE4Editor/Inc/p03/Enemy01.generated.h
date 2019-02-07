724699b329a85e7b4245ee060d6c212f3d029533
Cameron Mullins <1010934@student.sae.edu.au>
2019-02-07 15:44:58 +1100

    Add Back files

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/p03/Enemy01.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef P03_Enemy01_generated_h
+#error "Enemy01.generated.h already included, missing '#pragma once' in Enemy01.h"
+#endif
+#define P03_Enemy01_generated_h
+
+#define p03_Source_p03_Enemy01_h_12_RPC_WRAPPERS
+#define p03_Source_p03_Enemy01_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define p03_Source_p03_Enemy01_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEnemy01(); \
+	friend struct Z_Construct_UClass_AEnemy01_Statics; \
+public: \
+	DECLARE_CLASS(AEnemy01, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/p03"), NO_API) \
+	DECLARE_SERIALIZER(AEnemy01)
+
+
+#define p03_Source_p03_Enemy01_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAEnemy01(); \
+	friend struct Z_Construct_UClass_AEnemy01_Statics; \
+public: \
+	DECLARE_CLASS(AEnemy01, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/p03"), NO_API) \
+	DECLARE_SERIALIZER(AEnemy01)
+
+
+#define p03_Source_p03_Enemy01_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEnemy01(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemy01) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemy01); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemy01); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemy01(AEnemy01&&); \
+	NO_API AEnemy01(const AEnemy01&); \
+public:
+
+
+#define p03_Source_p03_Enemy01_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemy01(AEnemy01&&); \
+	NO_API AEnemy01(const AEnemy01&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemy01); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemy01); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEnemy01)
+
+
+#define p03_Source_p03_Enemy01_h_12_PRIVATE_PROPERTY_OFFSET
+#define p03_Source_p03_Enemy01_h_9_PROLOG
+#define p03_Source_p03_Enemy01_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	p03_Source_p03_Enemy01_h_12_PRIVATE_PROPERTY_OFFSET \
+	p03_Source_p03_Enemy01_h_12_RPC_WRAPPERS \
+	p03_Source_p03_Enemy01_h_12_INCLASS \
+	p03_Source_p03_Enemy01_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define p03_Source_p03_Enemy01_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	p03_Source_p03_Enemy01_h_12_PRIVATE_PROPERTY_OFFSET \
+	p03_Source_p03_Enemy01_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	p03_Source_p03_Enemy01_h_12_INCLASS_NO_PURE_DECLS \
+	p03_Source_p03_Enemy01_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID p03_Source_p03_Enemy01_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
